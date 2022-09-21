796e5b68c1eb542a39858edf005ad71393f9e69a
ali-zargari <77089778+ali-zargari@users.noreply.github.com>
2022-09-20 21:32:26 -0700

    Background bugs fixed. Logic is completed for background switching.
    Deployable

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,6 @@ module.exports = {
     output: {
         filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist'),
-        publicPath: "/Page/",
         clean: true,
     },
     module: {
